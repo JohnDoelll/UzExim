74c625cb418dc75f73045b3b45c2109b65b008fc
Eldor <eldorshodmonov55@gmail.com>
2022-09-10 14:47:25 +0500

    add files

---
+++ lang.js
@@ -0,0 +1,397 @@
+const langArr={
+    'home':{
+        'en':'Home',
+        'ru':'дом',
+        'uz':'Bosh sahifa'
+    },
+    'products':{
+        'en':'Products',
+        'ru':'товары',
+        'uz':'Mahsulotlar'
+    },
+    'products-2':{
+        'en':'Products',
+        'ru':'товары',
+        'uz':'Mahsulotlar'
+    },
+    'products-3':{
+        'en':'Products',
+        'ru':'товары',
+        'uz':'Mahsulotlar'
+    },
+
+    'about':{
+        'en':'About',
+        'ru':'около',
+        'uz':'Biz haqimizda'
+    },
+    'about-2':{
+        'en':'About',
+        'ru':'около',
+        'uz':'Biz haqimizda'
+    },
+
+    'contacts':{
+        'en':'Contacts',
+        'ru':'контакты',
+        'uz':'Kontaktlar'
+    },
+    'contact':{
+        'en':'Contacts',
+        'ru':'контакты',
+        'uz':'Kontaktlar'
+    },
+    'about-text':{
+        'en':`Welltec Machinery Limited has evolved and become one of the market leaders in the field. Our long-
+        established and proven partnerships with the industries players have reinforced our presence around the
+        world.`,
+        'ru':`Welltec Machinery Limited развивалась и стала одним из лидеров рынка в этой области. Наш долго-
+        установленные и проверенные партнерские отношения с игроками отрасли укрепили наше присутствие во всем мире.
+        Мир.`,
+        'uz':`Welltec Machinery Limited rivojlandi va bu sohada bozor yetakchilaridan biriga aylandi. Bizning uzoq -
+        sanoat ishtirokchilari bilan o'rnatilgan va tasdiqlangan hamkorlik bizning atrofimizdagi mavjudligimizni kuchaytirdi
+        dunyo.`
+    },
+
+    'auto':{
+        'en':'Automotive',
+        'ru':'Автомобильный',
+        'uz':'Avtomobilsozlik'
+    },
+    'auto-2':{
+        'en':'Automotive',
+        'ru':'Автомобильный',
+        'uz':'Avtomobilsozlik'
+    },
+    'auto-3':{
+        'en':'Automotive',
+        'ru':'Автомобильный',
+        'uz':'Avtomobilsozlik'
+    },
+    'auto-c':{
+        'en':'Driving force for development',
+        'ru':'Движущая сила развития',
+        'uz':'Rivojlanish uchun harakatlantiruvchi kuch'
+    },
+    'auto-c-1':{
+        'en':'Driving force for development',
+        'ru':'Движущая сила развития',
+        'uz':'Rivojlanish uchun harakatlantiruvchi kuch'
+    },
+    'to-read':{
+        'en':'To read more',
+        'ru':'Чтобы узнать больше',
+        'uz':`Batafsil`
+    },
+    'to-read-2':{
+        'en':'To read more',
+        'ru':'Чтобы узнать больше',
+        'uz':`Batafsil`
+    },
+    'to-read-3':{
+        'en':'To read more',
+        'ru':'Чтобы узнать больше',
+        'uz':`Batafsil`
+    },
+    'to-read-4':{
+        'en':'To read more',
+        'ru':'Чтобы узнать больше',
+        'uz':`Batafsil`
+    },
+    'to-read-5':{
+        'en':'To read more',
+        'ru':'Чтобы узнать больше',
+        'uz':`Batafsil`
+    },
+    'to-read-6':{
+        'en':'To read more',
+        'ru':'Чтобы узнать больше',
+        'uz':`Batafsil`
+    },
+    'cons':{
+        'en':'Construction',
+        'ru':'Строительство',
+        'uz':'Qurilish'
+    },
+    'cons-2':{
+        'en':'Construction',
+        'ru':'Строительство',
+        'uz':'Qurilish'
+    },
+    'cons-3':{
+        'en':'Construction',
+        'ru':'Строительство',
+        'uz':'Qurilish'
+    },
+    'cons-c':{
+        'en':'From durability to flexibility',
+        'ru':'От прочности к гибкости',
+        'uz':'Chidamlilikdan moslashuvchanlikgacha'
+    },
+    'cons-c-1':{
+        'en':'From durability to flexibility',
+        'ru':'От прочности к гибкости',
+        'uz':'Chidamlilikdan moslashuvchanlikgacha'
+    },
+    'elect':{
+        'en':'Electronics & Telecom',
+        'ru':'Электроника и Телекоммуникации',
+        'uz':'Elektron va telekom'
+    },
+    'elect-2':{
+        'en':'Electronics & Telecom',
+        'ru':'Электроника и Телекоммуникации',
+        'uz':'Elektron va telekom'
+    },
+    'elect-3':{
+        'en':'Electronics & Telecom',
+        'ru':'Электроника и Телекоммуникации',
+        'uz':'Elektron va telekom'
+    },
+    'elect-c':{
+        'en':'Ready for customisation',
+        'ru':'Готов к настройке',
+        'uz':'Moslashtirishga tayyor'
+    },
+    'elect-c-1':{
+        'en':'Ready for customisation',
+        'ru':'Готов к настройке',
+        'uz':'Moslashtirishga tayyor'
+    },
+    'food':{
+        'en':'Food & Packaging',
+        'ru':'Упаковка для еды',
+        'uz':'Oziq-ovqat va qadoqlash'
+    },
+    'food-2':{
+        'en':'Food & Packaging',
+        'ru':'Упаковка для еды',
+        'uz':'Oziq-ovqat va qadoqlash'
+    },
+    'food-3':{
+        'en':'Food & Packaging',
+        'ru':'Упаковка для еды',
+        'uz':'Oziq-ovqat va qadoqlash'
+    },
+    'food-c':{
+        'en':'Flexibility and Efficiency',
+        'ru':'Гибкость и эффективность',
+        'uz':'Moslashuvchanlik va samaradorlik'
+    },
+    'food-c-1':{
+        'en':'Flexibility and Efficiency',
+        'ru':'Гибкость и эффективность',
+        'uz':'Moslashuvchanlik va samaradorlik'
+    },
+    'modal-h':{
+        'en':'Our contacts',
+        'ru':'Наши контакты',
+        'uz':`Bizning kontaktlar`
+    },
+    'modal-c':{
+        'en':'Contact Us',
+        'ru':'Свяжитесь с нами',
+        'uz':`Biz bilan bog'laning`
+    },
+    'modal-c-2':{
+        'en':'Contact Us',
+        'ru':'Свяжитесь с нами',
+        'uz':`Biz bilan bog'laning`
+    },
+    'medi':{
+        'en':'Medical',
+        'ru':'Медицинский',
+        'uz':`Tibbiyot`
+    },
+    'medi-2':{
+        'en':'Medical',
+        'ru':'Медицинский',
+        'uz':`Tibbiyot`
+    },
+    'medi-3':{
+        'en':'Medical',
+        'ru':'Медицинский',
+        'uz':`Tibbiyot`
+    },
+    'medi-c':{
+        'en':'Utmost quality and reliability',
+        'ru':'Максимальное качество и надежность',
+        'uz':`Eng yuqori sifat va ishonchlilik`
+    },
+    'medi-c-1':{
+        'en':'Utmost quality and reliability',
+        'ru':'Максимальное качество и надежность',
+        'uz':`Eng yuqori sifat va ishonchlilik`
+    },
+    'tech':{
+        'en':'Technical Moulding',
+        'ru':'Техническое литье',
+        'uz':`Texnik qoliplash`
+    },
+    'tech-2':{
+        'en':'Technical Moulding',
+        'ru':'Техническое литье',
+        'uz':`Texnik qoliplash`
+    },
+    'tech-3':{
+        'en':'Technical Moulding',
+        'ru':'Техническое литье',
+        'uz':`Texnik qoliplash`
+    },
+    'tech-c':{
+        'en':'Rich experience in technical moulding',
+        'ru':'Богатый опыт в области технического литья',
+        'uz':`Texnik qoliplash bo'yicha boy tajriba`
+    },
+    'tech-c-1':{
+        'en':'Rich experience in technical moulding',
+        'ru':'Богатый опыт в области технического литья',
+        'uz':`Texnik qoliplash bo'yicha boy tajriba`
+    },
+    'social':{
+        'en':'Get connected with us on social networks:',
+        'ru':'Присоединяйтесь к нам в социальных сетях:',
+        'uz':`Ijtimoiy tarmoqlarda biz bilan bog'laning:`
+    },
+    'company-name':{
+        'en':'Company name',
+        'ru':'Название компании',
+        'uz':`Kopmaniya nomi`
+    },
+    'company-c':{
+        'en':`Established in 1982, we started to manufacture injection moulding machines in Hong Kong China.
+        Welltec Machinery Limited has evolved and become one of the market leaders in the field. Our long- established and proven partnerships with the industries players have reinforced our presence around the world.`,
+        'ru':`Основанная в 1982 году, мы начали производить машины для литья под давлением в Гонконге, Китай.
+        Welltec Machinery Limited развивалась и стала одним из лидеров рынка в этой области. Наши давние и проверенные партнерские отношения с игроками отрасли укрепили наше присутствие по всему миру.`,
+        'uz':`1982 yilda tashkil etilgan biz Gonkong Xitoyda inyeksion kalıplama mashinalarini ishlab chiqarishni boshladik.
+        Welltec Machinery Limited rivojlandi va bu sohada bozor yetakchilaridan biriga aylandi. Bizning sanoat ishtirokchilari bilan uzoq vaqtdan beri o'rnatilgan va tasdiqlangan hamkorligimiz butun dunyo bo'ylab mavjudligimizni mustahkamladi.`
+    },
+
+    'usef':{
+        'en':' Useful links',
+        'ru':'Полезные ссылки',
+        'uz':`Foydali havolalar`
+    },
+
+
+    'pric':{
+        'en':'Pricing',
+        'ru':'Цены',
+        'uz':`Narxlash`
+    },
+    'sett':{
+        'en':'Settings',
+        'ru':'Настройки',
+        'uz':`Sozlamalar`
+    },
+     'orde':{
+        'en':'Orders',
+        'ru':'Заказы',
+        'uz':`Buyurtmalar`
+    }, 
+    'help':{
+        'en':'Help',
+        'ru':'Помощь',
+        'uz':`Yordam`
+    },
+    'modal-a':{
+        'en':`The automotive industry has been a key driving force for the technology development in
+        Welltec.
+        Over the years, we have been a competent partner for automobile industries, known for
+        high precision, sophisticated components, high- strength parts, light-weight composites,
+        smart interior and exterior surfaces.`,
+        'ru':`Автомобильная промышленность была ключевой движущей силой развития технологий в
+        Welltec.
+        На протяжении многих лет мы являемся компетентным партнером автомобильной промышленности, известным
+        высокая точность, сложные компоненты, высокопрочные детали, легкие композиты,
+        умные внутренние и внешние поверхности.`,
+        'uz':`Avtomobilsozlik sanoati texnologiyani rivojlantirish uchun asosiy harakatlantiruvchi kuch bo'ldi
+        Welltec.
+        Yillar davomida biz taniqli avtomobil sanoati uchun vakolatli hamkor bo'ldik
+        yuqori aniqlikdagi, murakkab komponentlar, yuqori quvvatli qismlar, engil kompozitlar,
+        aqlli ichki va tashqi yuzalar.`
+    },
+    'modal-co':{
+        'en':`Plastics are often the versatile choice when it comes to Construction.
+        Welltec understands the construction industry needs for durable, low weight, high
+        flexibility and weather- resistant components. Our processing technology enables you to
+        keep pace with this field.`,
+        'ru':`Пластмассы часто являются универсальным выбором, когда дело доходит до строительства.
+        Welltec понимает потребности строительной отрасли в прочных, легких,
+        гибкость и погодостойкие компоненты. Наша технология обработки позволяет
+        идти в ногу с этой области.`,
+        'uz':`Qurilish haqida gap ketganda, plastmassa ko'pincha ko'p qirrali tanlovdir.
+        Welltec qurilish industriyasining bardoshli, past og'irligi va yuqori bo'lishi kerakligini tushunadi
+        moslashuvchanlik va ob-havoga chidamli komponentlar. Bizning qayta ishlash texnologiyamiz sizga imkon beradi
+        bu soha bilan hamqadam bo'ling.`
+    },
+    'modal-e':{
+        'en':`The ongoing technical challenges in Electronics and Telecommunications are the ability
+        to respond fast, offer higher customisation processes, such as delivering
+        fully-automated casings and covers of different geometries.
+        Welltec continues to evolve and keep pace with this dynamic field.
+        Our technologies, in rapid product development cycles, offer you products of better
+        precision, quicker cycle times and cutting-edge production solutions.`,
+        'ru':`Текущие технические проблемы в области электроники и телекоммуникаций заключаются в способности
+        чтобы быстро реагировать, предлагайте более сложные процессы настройки, такие как доставка
+        полностью автоматизированные кожухи и крышки различной геометрии.
+        Welltec продолжает развиваться и идти в ногу с этой динамичной областью.
+        Наши технологии в быстрых циклах разработки продукта предлагают вам продукты лучшего качества.
+        точность, сокращение времени цикла и передовые производственные решения.`,
+        'uz':`Elektronika va telekommunikatsiya sohasida davom etayotgan texnik muammolar qobiliyatdir
+        tezkor javob berish uchun, yetkazib berish kabi yuqori moslashtirish jarayonlarini taklif qiling
+        to'liq avtomatlashtirilgan korpuslar va turli geometriyali qopqoqlar.
+        Welltec rivojlanishda davom etmoqda va ushbu dinamik maydon bilan hamnafas bo'ladi.
+        Bizning texnologiyalarimiz tezkor mahsulotlarni ishlab chiqish davrlarida sizga yaxshiroq mahsulotlarni taklif qiladi
+        aniqlik, tezroq aylanish vaqtlari va ilg'or ishlab chiqarish echimlari.`
+    },
+    'modal-f':{
+        'en':` Higher standards are increasingly required for food and packaging industry in terms of
+        thinner wall, precision, cost-efficient and perfectly designed articles which are fit
+        for logistics.High-speed machines play a vital role in the packaging field. Over the years, Welltec
+        has worked to deliver shorter cycle time with robust machines.
+        We are your competent partner to offer you the turnkey solutions for quality packaging
+        items in different size and shape.`,
+        'ru':`Для пищевой и упаковочной промышленности все чаще требуются более высокие стандарты с точки зрения
+        более тонкие стенки, точные, экономичные и идеально спроектированные изделия, которые подходят
+        для логистики. Высокоскоростные машины играют жизненно важную роль в области упаковки. На протяжении многих лет Welltec
+        работал над сокращением времени цикла с помощью надежных машин.
+        Мы являемся вашим компетентным партнером, который предлагает готовые решения для качественной упаковки.
+        предметы разного размера и формы.`,
+        'uz':`Oziq-ovqat va qadoqlash sanoati uchun yanada yuqori standartlar talab qilinmoqda
+        yupqaroq devor, mos keladigan aniq, tejamkor va mukammal ishlab chiqilgan buyumlar
+        logistika uchun. Yuqori tezlikda ishlaydigan mashinalar qadoqlash sohasida muhim rol o'ynaydi. Yillar davomida Welltec
+        mustahkam mashinalar bilan qisqaroq aylanish vaqtini ta'minlash uchun ishladi.
+        Biz sizga sifatli qadoqlash bo'yicha kalit yechimlarni taklif qilish uchun sizning vakolatli hamkoringizmiz
+        turli o'lcham va shakldagi buyumlar.`
+    },
+    'modal-m':{
+        'en':`Our injection moulding machines are compatible with clean rooms and sterile products.
+        Welltec will always help you succeed towards the specific requirements of the sensitive
+        medical industry.`,
+        'ru':`Наши машины для литья под давлением совместимы с чистыми помещениями и стерильными продуктами.
+        Welltec всегда поможет вам добиться успеха в выполнении специфических требований чувствительных
+        медицинская промышленность.`,
+        'uz':`Bizning inyeksion kalıplama mashinalarimiz toza xonalar va steril mahsulotlarga mos keladi.
+        Welltec har doim sezgirlarning o'ziga xos talablariga erishishda sizga yordam beradi
+        tibbiyot sanoati.`
+    },
+    'modal-t':{
+        'en':`Welltec has accumulated a wealth of experience in a vast array of technical moulding
+        across toys, pallets, large containers, household and white goods, etc.
+        We enable you to produce the innovative products for your market.`,
+        'ru':`Welltec накопила богатый опыт в области технического литья.
+        через игрушки, поддоны, большие контейнеры, бытовую и бытовую технику и т. д.
+        Мы даем вам возможность производить инновационные продукты для вашего рынка.`,
+        'uz':`Welltec juda ko'p texnik qoliplash bo'yicha boy tajriba to'plagan
+        o'yinchoqlar, tagliklar, katta konteynerlar, uy-ro'zg'or buyumlari va oq buyumlar va boshqalar bo'ylab.
+        Biz sizga bozoringiz uchun innovatsion mahsulotlarni ishlab chiqarish imkonini beramiz.`
+    },
+
+    '':{
+        'en':``,
+        'ru':``,
+        'uz':``
+    }
+    
+}
